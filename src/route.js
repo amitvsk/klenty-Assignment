9a116913acc0d4162e97c569c15618f8e71ef904
amitvsk <83784108+amitvsk@users.noreply.github.com>
2022-10-13 20:56:22 +0530

    Update route.js

--- src/route.js
+++ src/route.js
@@ -9,6 +9,7 @@ router.post('/userRegister',userController.userRegister);
 router.post('/Login',userController.userLogin);
 
 /********* User typing Api ***********/
+router.get('/',(req,res)=>{return res.send('hello klenty')});
 router.post('/testTyping/:userId',Authentication,Authorization,typingController.userTyping);
 router.get('/getUserById/:userId',Authentication,Authorization,typingController.getTypingById);
 router.get('/',typingController.getTypinglist);
